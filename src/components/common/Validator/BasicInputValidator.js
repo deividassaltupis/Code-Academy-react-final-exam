2176e6679718912df8ff856a918094da8f8a4dc9
Deividas Šaltupis <deividassaltupis18@gmail.com>
2021-11-17 11:54:27 +0200

    No-unused vars error fix (Netlify build fails)

--- src/components/common/Validator/BasicInputValidator.js
+++ src/components/common/Validator/BasicInputValidator.js
@@ -1,9 +1,7 @@
-import React from "react";
-
 const BasicInputValidator = ({ render, inputValue, submitClicked }) => {
-    let invalid = false;
-    if (submitClicked && !inputValue.length) invalid = true;
-    return render(invalid);
+  let invalid = false;
+  if (submitClicked && !inputValue.length) invalid = true;
+  return render(invalid);
 };
 
 export default BasicInputValidator;
